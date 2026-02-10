3281dac914c49f3ad759983e291866cf23497cd7
SJLee-03 <sjleego03@gmail.com>
2026-02-10 17:52:26 +0900

    version2

---
+++ basic_syntax/2_env_setting.sh
@@ -0,0 +1,16 @@
+# github인증방법 2가지
+# 1. oauth인증방식 (제3자인증-웹로그인)
+# 2. pat token인증방식 : github에서 직접 보안키 qkfrmq
+# 발급한 키를 키체인(자격증명)에 등록
+
+# git 프로젝트 생성방법 2가지
+# 방법1. github에서 진행중인 원격 repo를 clone
+# 방법2. 로컬에서부터 이미 진행중인 프로젝트를 github에 업로드
+# 해당 프로젝트 위치에 .git 폴더(repo주소, 사용자 정보 등) 생성
+git init
+# 원격지 주소를 생성 및 추가
+git remote add origin 레포주소
+# 원격지 주소 삭제
+git remote remove origin
+# 원격지 주소 변경
+git remote set-url origin 레포주소 
